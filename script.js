8cc8549052b652f9fd8319f11628433acfd28b63
Agata Snoch <agatasnoch@Agatas-MacBook-Air.local>
2017-08-07 15:39:39 +0400

    zadanie funkcje

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function getTriangleArea - function getTriangleArea(a, h) {
+    var a = 5;
+    var h = 10;
+    
+    if ( a >= 0 || h >= 0 ) {
+    console.log('Nieprawidlowe dane');
+    }
+    
+    
+
+}
+
+function getTriangleArea - function getTriangleArea(a, h) {
+    
+    var result = a*h/2;
+     return result;
+}
+   console.log( getTriangleArea(10,6) )
